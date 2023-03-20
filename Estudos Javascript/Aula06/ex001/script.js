25309388a4b9b75304309d8972d17ee5cb12d6c3
Leandro wilker <leandrowilker99@gmail.com>
2023-03-19 23:38:34 -0300

    add curso em video

---
+++ Estudos Javascript/Aula06/ex001/script.js
@@ -0,0 +1,4 @@
+window.alert('Minha primeira mensagem')
+window.confirm('Você está gostando de Js?')
+var nome = window.prompt('Qual o seu nome?')
+window.alert(`é um prazer em te conhecer, ${nome}!`)
---
+++ Estudos Javascript/Aula06/ex002/script.js
@@ -0,0 +1,6 @@
+window.confirm('Vamos somar?')
+var n1 = Number(window.prompt('Digite um número')) //pode-se usar Number.ParseInt para números inteiros ou Number.ParseFloat para números flutuantes ou apenas Number ele ja detecta automaticamente.
+var n2 = Number(window.prompt('Digite outro número'))//caso queira converter de número para String é só usar String(n2) ou a variavél exemplo: n2.toString()
+var total = n1+n2
+window.alert(`a soma entre ${n1} + ${n2} é igual à ${total}`)
+Number.parseInt
